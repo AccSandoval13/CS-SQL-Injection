80a795768cefb476cd211ee42f6ceb1e497e3136
Ricardo Sandovale <AccSandoval13@gmail.comm>
2023-04-30 22:26:19 -0700

    Updated Files

--- app.js
+++ app.js
@@ -43,7 +43,6 @@ app.post('/login', function (req, res) {
 			res.send('Hello <b>' + row.title + '!</b><br /> This file contains all your secret data: <br /><br /> SECRETS <br /><br /> MORE SECRETS <br /><br /> <a href="/index.html">Go back to login</a>');
 		}
 	});
-
 });
 
 app.listen(3000);
